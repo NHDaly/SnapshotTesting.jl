65f9ac8db4e1d3c01d4a411d85d55a45c2c048b6
Nathan Daly <nhdaly@gmail.com>
2022-07-21 11:55:29 -0600

    Files generated by PkgTemplates

---
+++ src/SnapshotTesting.jl
@@ -0,0 +1,5 @@
+module SnapshotTesting
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using SnapshotTesting
+using Test
+
+@testset "SnapshotTesting.jl" begin
+    # Write your tests here.
+end
